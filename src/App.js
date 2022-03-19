4170c2ed83fe95ea762abc4b6723e8790a17cce3
kimsungsik03D <tjdtlr2620@gmail.com>
2022-03-20 00:44:22 +0900

    Route 설치

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import SoloInGameInterface from './component/single/SoloInGameInterface';
+import Main from './component/main/Main';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+   <Router>
+     <Routes>
+       <Route path="/" element={<Main />} />
+       <Route path="/SoloInGameInterface" element={<SoloInGameInterface />} />
+     </Routes>
+
+   </Router>
   );
 }
 
