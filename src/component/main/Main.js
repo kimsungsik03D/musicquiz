54fe8f499f545a6ed0ff3feacd3811d39074fd58
kimsungsik03D <tjdtlr2620@gmail.com>
2022-03-20 01:06:17 +0900

    메인페이지에 인게임 링크 추가

--- src/component/main/Main.js
+++ src/component/main/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Main(){
   return(
     <div>
-      메인페이지입니다.
+      <div>메인페이지입니다.</div>
+      <div><Link to={'SoloInGameInterface'}>인게임</Link></div>
     </div>
   )
 }
