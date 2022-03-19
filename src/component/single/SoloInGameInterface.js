9dd2f5ed65f2a819031801c2bee712b1c1f40dd3
kimsungsik03D <tjdtlr2620@gmail.com>
2022-03-20 00:37:41 +0900

    싱글 인게임 페이지 생성

---
+++ src/component/single/SoloInGameInterface.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function SoloInGameInterface(){
+  return(
+    <div>
+      <div>
+        <div>솔로</div>
+        <div>사용자이름</div>
+        <div>문제년도 - 문제년도</div>
+      </div>
+    </div>
+  )
+}
