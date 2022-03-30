3bb192aceb612f5745d0e2d05ce4e845b6196b62
kimsungsik03D <tjdtlr2620@gmail.com>
2022-03-30 21:58:03 +0900

    메인 - 기본요소만 구성 추가적으로 link 연결 및 CSS작업 해야함

--- src/component/main/Main.js
+++ src/component/main/Main.js
@@ -5,7 +5,13 @@ export default function Main(){
   return(
     <div>
       <div>메인페이지입니다.</div>
-      <div><Link to={'SoloInGameInterface'}>인게임</Link></div>
+      <div><input type={'text'}/> </div>
+      <div style={{display : 'flex'}}>
+        <div><Link to={'SoloInGameInterface'}>솔로</Link></div>
+        <div><Link to={'#'}>멀티</Link></div>
+        <div><Link to={'#'}>사용자설정</Link></div>
+      </div>
+      <div><Link to={'SoketTest'}>소켓테스트</Link></div>
     </div>
   )
 }
