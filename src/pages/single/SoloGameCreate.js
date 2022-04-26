dc87afee2bc8e543fdec50902ccbf706330ca356
kimsungsik03D <tjdtlr2620@gmail.com>
2022-04-27 00:19:24 +0900

    방만들기 - props추가, 데이터 포멧 및 초기화 설정면경, props를 통한 websokect구성, input 항목에따른 자료형 지정, sokect send 구현

--- src/pages/single/SoloGameCreate.js
+++ src/pages/single/SoloGameCreate.js
@@ -2,35 +2,69 @@ import React, { useEffect, useState } from 'react'
 import './SoloInGameCreate.css'
 import { Link, useLocation } from 'react-router-dom'
 
-const SoloGameCreate = () => {
+const SoloGameCreate = (props) => {
     const location = useLocation()
     const state = location.state
     const [input, setInput] = useState({
-        // userName:  state?state.name:'' ,
-        userName:  state.userName,
-        toYear: '',
-        fromYear: '',
-        questionCount: '',
-        modtype: '',
-        songHint: '',
-        singerHint: '',
+        "userName" : state.name?state.name:"홍길동",
+        "toYear" : null,
+        "fromYear" : null,
+        "questionCount" : null,
+        "modtype" : '',
+        "songHint" : '',
+        "singerHint" : '',
+        "rankMod" : true
     })
     //화면에서 사용하기 위한 변수를 선언해 비구조화할당을 통해 해당 변수에 Input 값을 추출한다.
     const { userName,toYear, fromYear, questionCount, singerHint, songHint } = input
 
+    // let ws = null
+    // ws = new WebSocket(`ws://13.125.191.54:8088/websocket`)
+    // ws = new WebSocket(`ws://13.125.191.54:8088/websocket`)
+    props.props.onopen = (e) => {
+        console.log('connected!!')
+    }
+    props.props.onmessage = (e) => {
+        console.log(JSON.parse(e.data));
+        console.log('onmessage!!')
+    }
+    props.props.onclose = (e) => {
+        console.log('onclose');
+    }
+
     function onChange(e) {
         const { value, name } = e.target
-        setInput({
-            ...input,
-            [name]: value,
-        })
+        // number일경우 형변환
+        if(e.target.name=='toYear'||e.target.name=='fromYear'||e.target.name=='questionCount'){
+            setInput({
+                ...input,
+                [name]: Number(value),
+            })
+        }else{// number가 아닐경우 boolean으로 표현하기위해 삼항연산자 활용
+            setInput({
+                ...input,
+                [name]: value=='true'?true:false,
+            })
+        }
     }
 
     function onClickGoHome() {
         window.location.href = '/'
     }
 
-
+    const onClickMessage= (e)=>{
+        // var msg = {
+        //     "userName" : "message",
+        //     "toYear" : 2000,
+        //     "fromYear" : 2020,
+        //     "questionCount" :2 ,
+        //     "modtype" :true,
+        //     "songHint" : true,
+        //     "singerHint" : true,
+        //     "rankMod" : true
+        // }
+        props.props.send(JSON.stringify(input))
+    }
     return (
         <div className={'mainContainer'}>
             <div className={'SoloHeader'}>
@@ -51,15 +85,22 @@ const SoloGameCreate = () => {
                         <div>힌트 설정</div>
                         <div>
                             노래 가수 힌트 :{' '}
-                            <input type={'radio'} name={'singerHint'} id={'singernameonpen'} value={'open'} onClick={onChange} />{' '}공개{' '}
-                            <input type={'radio'} name={'singerHint'} id={'singernameoclose'} value={'close'} onClick={onChange} />{' '}
+                            <input type={'radio'} name={'singerHint'} id={'singernameonpen'}  value={true} onClick={onChange} />{' '}공개{' '}
+                            <input type={'radio'} name={'singerHint'} id={'singernameoclose'} value={false} onClick={onChange} />{' '}
                             비공개
                         </div>
                         <div>
                             노래 초성 힌트 :{' '}
-                            <input type={'radio'} name={'songHint'} id={'singertitleonpen'} value={'open'} onClick={onChange} />{' '}
+                            <input type={'radio'} name={'songHint'} id={'singertitleonpen'}  value={true} onClick={onChange} />{' '}
                             공개{' '}
-                            <input type={'radio'} name={'songHint'} id={'singertitleoclose'} value={'close'} onClick={onChange} />{' '}
+                            <input type={'radio'} name={'songHint'} id={'singertitleoclose'} value={false} onClick={onChange} />{' '}
+                            비공개
+                        </div>
+                        <div>
+                            노래 초성 힌트 :{' '}
+                            <input type={'radio'} name={'modtype'} id={'singertitleonpen'}  value={true} onClick={onChange} />{' '}
+                            공개{' '}
+                            <input type={'radio'} name={'modtype'} id={'singertitleoclose'} value={false} onClick={onChange} />{' '}
                             비공개
                         </div>
                     </div>
@@ -75,7 +116,7 @@ const SoloGameCreate = () => {
                 <div>
                     <Link to="/SoloInGameInterface" state={input}>
                         {/*TODO onClick 이벤트를 통해 서버로 같이 넘겨주기.*/}
-                        <input type={'button'} value={'게임시작'} />
+                        <input type={'button'} value={'게임시작'} onClick={onClickMessage}/>
                     </Link>
                 </div>
             </div>
