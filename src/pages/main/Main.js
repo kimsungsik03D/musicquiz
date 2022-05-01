5128ab9a4594d8f063ad831888b8b74f50696aa2
kimsungsik03D <tjdtlr2620@gmail.com>
2022-05-01 20:33:30 +0900

    메인화면 구성전 임시 솔로 랭킹 구성

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,11 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
+import { type } from '@testing-library/user-event/dist/type';
 
 export default function Main() {
     const [userName, setUserName] = useState('')
     function onChangeUserName(e) {
         setUserName(e.target.value)
     }
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get(
+        'http://13.125.191.54:8088/rankList'
+      );
+      setUsers(response.data.rankList); // 데이터는 response.data 안에 들어있습니다.
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
     return (
         <div>
             <div>메인페이지입니다.</div>
@@ -30,6 +49,34 @@ export default function Main() {
             <div>
                 <Link to={'SoketTest'}>소켓테스트</Link>
             </div>
+          <table border={1}>
+            <thead>
+            <tr>
+              <th colSpan={4}>솔로모드 랭킹</th>
+            </tr>
+            </thead>
+
+            <tr>
+              <td>순번</td>
+              <td>이름</td>
+              <td>점수</td>
+              <td>게임시간</td>
+            </tr>
+            {/*{rankListData}asdf*/}
+            {/*{console.log('users',users)}*/}
+            {users?users.map((data,index) => <tr
+              key={index}>
+                <td>{data.row}</td>
+                <td>{data.username}</td>
+                                        <td>{data.score}</td>
+
+                                        <td>{(data.cleartime/1000).toFixed(2)}초</td>
+                                     </tr>)
+                                        :
+                                        ''
+            }
+
+          </table>
         </div>
     )
 }
